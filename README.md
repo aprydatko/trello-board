fbbc64ee974d66c9df6f1cb9b8c2c78dc711a9b9
Artur Prydatko <artyrpridatko@gmail.com>
2025-02-27 16:50:25 +0200

    finish project

---
+++ README.md
@@ -0,0 +1,23 @@
+# Drag-end-drop Trello Board Vue
+
+Look how it works online [TrelloBoard](https://nuxt.com/docs/getting-started/introduction).
+
+## Setup
+
+Make sure to install dependencies:
+
+```bash
+# npm
+npm install
+
+# pnpm
+pnpm install
+
+# yarn
+yarn install
+
+# bun
+bun install
+```
+
+##
---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
+    <h1 class="text-4xl text-white flex items-center mb-10">
+      <img
+        width="45"
+        class="mr-3"
+        src="https://cdn.worldvectorlogo.com/logos/trello.svg"
+        alt="Trello logo"
+      />
+      Trello Board
+    </h1>
+    <TrelloBoard />
+  </div>
+</template>
